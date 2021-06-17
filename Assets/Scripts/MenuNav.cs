6c8a43c51ea5fb1a2e2d03e20e3400848fa27515
Kolppan3n <7803790+Kolppan3n@users.noreply.github.com>
2021-06-17 15:10:50 +0300

    Created Game Scene

--- Assets/Scripts/MenuNav.cs
+++ Assets/Scripts/MenuNav.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManager;
+using UnityEngine.SceneManagement;
 
 public class MenuNav : MonoBehaviour
 {
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+
     }
 }
